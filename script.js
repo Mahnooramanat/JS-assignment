ece1aa63175fc3641119212fa7f396de1da99ed3
Mahnooramanat <145896398+Mahnooramanat@users.noreply.github.com>
2023-10-06 23:03:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Practice Exercise no 1.1
+console.log("Mahnoor Amanat Ali!");
+// Practice Exercise 1.2(done in index.html)
+alert("Saying hi from a different file!");
+// Practice Exercise 1.3 (Yes JS code ran properly)
+// Practice Exercise 1.4
+console.log(a=10);
+ prompt("Hi!  How are you?");
+//  ITS JUST FOR MY PRACTICE
+console.log(Math.random()* 10);
+console.log(Math.random(10+4));
+console.log(Math.floor(Math.random()*100));
+// console.log("hellow world");
+console.log(30+20+4);
+// SELF-CHECK QUIZ
+console.log("(1)What is the HTML syntax to add an external JavaScript file?");
+console.log("(*)To include an external Javascript file,we can use the script tag with the attribute src.The script tag should be included between the <head>tags in your HTML document. ");
+console.log("(2)Can you run Javascript in a file with a JS extension in your browser?");
+console.log("(*)To execute Javascript in a browser you have two options-either put it inside a script element anywhere inside an HTML document,or put it inside an external Javascript file(with a.js extension) & then reference that file inside the HTML document using an empty script element with a src attribute.");
+console.log("(3)How do you write a multiple-line comment in Javascript?");
+console.log("(*)Javascript multiline comments,also known as block comments,start with a forward slash followed by an asterisk(/*)and end with an asterisk followed by a forward slash(*/).");
+console.log("(4)What is the best way to remove a line of code from running that you might want to keep as you debug?");
+console.log("(*)The best way to temporarily disable a line of code from running while keeping it for debugging  purposes is to comment it out.You can add(for single-line comments)at the beginning of the line you want to disable,or you can wrap the code block in and for multiple lines.");
